94756866ee77c7201e05620221adc76066d93711
rusiper1000 <rusiper1000@gmail.com>
2026-04-18 18:53:27 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'btc-app-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['./']))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
